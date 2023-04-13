7da1c432fc3988d9ccb428a05e8f904c6fc6cb3c
Aruvel <127906658+Aruvel@users.noreply.github.com>
2023-04-13 15:10:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.calxmlmpfagm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yongchuan-employee"
+customregion = "us-east-1"
